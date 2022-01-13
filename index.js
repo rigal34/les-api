2c2d58bb09acd88da353f6ff2e1cb71dcab75e4e
ALBATOR <SST.plomberie@laposte.net>
2022-01-13 20:39:43 +0100

    json and storage coockie

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ req.onload = reqListener ;//au chargement tu m'envois un fonction " reqListener"
 //FETCH = va chercher
 //LESSON les API le fectch 15min 31s
  //une fois que l'on a été cherché (alors,ensuite) on créer une promesse (response)avec une fonction fléchée
-// fetch( "https://api.blablagues.net/?rub=blagues")
-// .then((response) =>{
-//    //console.log(response);
+//  fetch( "https://www.theguardian.com/environment/climate-crisis")
+//  .then((response) =>{
+//    console.log(response);
 
-//    })
-//    .catch((err)=> console.log(err));//une fois que la fonction est exécutée tu attrapes l'erreur et tu la log
+//     })
+//     .catch((err)=> console.log(err));//une fois que la fonction est exécutée tu attrapes l'erreur et tu la log
 
 // fetch("data.txt")
 // .then((res) =>{
@@ -67,7 +67,7 @@ req.onload = reqListener ;//au chargement tu m'envois un fonction " reqListener"
 // il m'affiche le dossier data.json en log
 fetch("data.json")
 .then((res) => res.json())
-//.then((data) => console.log(data));
+.then((data) => console.log(data));
 
 //lesson API 27 MIN 25S
 
@@ -81,7 +81,7 @@ header: myHeaders,
 mode:"cors",//ce qui gére les permissions pour faire des requêtes
 Cache:"default"
 };
-fetch("data.json",init)
+ fetch("data.json",init)
 .then((res) => 
 console.log(res));
 
@@ -147,3 +147,118 @@ async function fetchData() {
    //attend que le await soit exécuté avant de faire la suite
       executeFonction();
  }
+
+ // ================================================================================================================================================================================================================================================================================
+ //                L      EEEEE         JJJ   SSS    OOO   N   N
+ //      /      /  L      E              J   S      O   O  NN  N
+ //     /      /   L      EEE            J    SSS   O   O  N N N
+ //    /      /    L      E              J       S  O   O  N  NN
+ //   /      /     L      E           J  J       S  O   O  N   N
+ //  /      /      LLLLL  EEEEE        JJ    SSSS    OOO   N   N
+ // ================================================================================================================================================================================================================================================================================
+ // * * * //LE JSON est un format pour transiter des données
+
+//Méthode.json() => cette méthode s'auto_résout en renvoyant le body de la requête au format json
+
+//pour aller chercher un fichier .json
+//il va chercher json.txt
+//il exécute la response de la méthode json
+// il m'affiche le dossier data.json en log
+//et dans mon 2iém log il transforme les données objet  json en string chaîne de caractére
+
+// fetch("data.json")
+// .then((res) => res.json())//cette méthode permet de résoudre le body
+// .then((data) => {
+// console.log(data);
+// console.log((JSON.stringify(data)));JSON.stringify covertit en JSON d'aprés le prof
+// });
+
+
+fetch("data.json")
+.then((res) => res.json())
+.then((data) => {
+   //stringify => convertit en JSON
+   let settings = JSON.stringify(data);
+   //la méthode Parse => transforme json en objet javascript
+   //console.log(JSON.parse(settings));
+ });
+
+
+ //----------------------------------------------
+ // ========================================================================================================================================================================================================================================================================================================
+ //                L      EEEEE   SSS        W   W  EEEEE  BBB           A    PPPP   III
+ //      /      /  L      E      S           W   W  E      B  B         A A   P   P   I
+ //     /      /   L      EEE     SSS        W   W  EEE    BBBB        A   A  P   P   I
+ //    /      /    L      E          S       W W W  E      B   B       AAAAA  PPPP    I
+ //   /      /     L      E          S       W W W  E      B   B       A   A  P       I
+ //  /      /      LLLLL  EEEEE  SSSS         W W   EEEEE  BBBB        A   A  P      III
+ // ========================================================================================================================================================================================================================================================================================================
+ // * * * //Les Web API
+//Ce sont des API que l'on a de base dans javascript ce sont les API du navigateur qui sont (canvas,location,history,local storage  client storage)
+
+//CLIENT STORAGE à l'intérieur on a le LOCAL STORAGE
+
+//La capacité de stockage storage est plus importante que celle des cookies
+
+//cookie stoctk 4 Ko
+
+//local storage stoctk 10 Mo les données ne transite pas avec la personne qui a fait le site
+
+//local storage est un objet de windows
+
+localStorage.monCoffre = "Je stock la data";
+
+//je vais recupérer la data pour l'afficher dans ma console
+
+//console.log(localStorage.monCoffre);
+document.body.textContent = localStorage.monCoffre;
+
+
+//pour supprimmer l'élément dans le local storage
+localStorage.removeItem("monCoffre");
+
+//LES API 2H12m
+//mettre à jour un local storage
+localStorage.mettreAJour = "Denis";
+
+//par contre on ne peux passer un objet un tableau dans le local storage par contre on peut utilser une méthode pour mettre en chaîne de caractére pour pouvoir faire passer mon objet
+
+
+const unObjet = {
+   name:"David",
+   age: 22,
+};
+//une fois convertit je le récupére pour l'interpréter en JSON
+localStorage.autreUsager = JSON.stringify(unObjet);
+
+
+console.log(localStorage.autreUsager);//{"name":"David","age":22}
+
+//un fois récupéré et convertit en javascript
+//console.log(JSON.parse(localStorage.autreUsager));//{name: 'David', age: 22}
+
+
+//SESSION STORAGE c'est comme le local storage joue sur la durée de stockage uniquement quand on est sur la page des que la ^page est fermée les donnée sont perdues
+
+sessionStorage.reglageDonee = "55px";
+//console.log(sessionStorage.reglageDonee);//et aprés si je veux je peux aussi le stocker dans un variable l'afficher à l'écran
+
+//pour ce le supprimer
+
+sessionStorage.clear();
+
+
+
+
+//les COOKIES
+// C'est quelque chose qui est envoyé d'un serveur vers un navigateur,ensuite le cookies il est stocké par le navigateur
+//je vais m'ajouter un cookies
+document.cookie = "username=Florian";
+
+//une bonne patrique à faire sur le cookie lesson las API 2h22
+
+document.cookie = "pseudo=FS;patch=/; max-age=450000; secure; samesite";
+
+document.cookie = "pseudo=FS;patch=/; expire=Thu,31 Dec 2099 23:59:59 GMT; secure; samesite";
+                  
+
